02cbb0a2dbcdd8efd80e8e8136416d5187970d71
Tomas McGuinness <tomas@chewedpencil.net>
2012-07-09 17:01:57 +0100

    Added route and method for returning an updated pass.

--- Passbook.Sample.Web/App_Start/RouteConfig.cs
+++ Passbook.Sample.Web/App_Start/RouteConfig.cs
@@ -16,18 +16,23 @@ namespace Passbook.Sample.Web
 
       //api/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier/serialNumber
       routes.MapHttpRoute(
-          name: "Registration",
+          name: "RegistraterPass",
           routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}",
           defaults: new { Controller = "PassRegistration", Action = "Post" });
 
-      ///api/v1/devices/d754ba022bd37181fed36ead4e7cf5d8/registrations/pass.tomasmcguinness.com
+      //api/v1/devices/d754ba022bd37181fed36ead4e7cf5d8/registrations/pass.tomasmcguinness.com
       routes.MapHttpRoute(
-          name: "PassFetchUpdatedPasses",
+          name: "FetchUpdatedPasses",
           routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}",
           defaults: new { Controller = "PassRegistration", Action = "Get" }
       );
 
-
+      //api/version/passes/passTypeIdentifier/serialNumber
+      routes.MapHttpRoute(
+          name: "FetchPass",
+          routeTemplate: "api/{version}/passes/{passTypeIdentifier}/{serialNumber}",
+          defaults: new { Controller = "PassRegistration", Action = "GetPass" }
+      );
 
       routes.MapRoute(
           name: "Default",
--- Passbook.Sample.Web/Controllers/PassRegistrationController.cs
+++ Passbook.Sample.Web/Controllers/PassRegistrationController.cs
@@ -9,21 +9,27 @@ using Newtonsoft.Json;
 
 namespace Passbook.Sample.Web.Controllers
 {
-    public class PassRegistrationController : ApiController
+  public class PassRegistrationController : ApiController
+  {
+    //https://webServiceURL/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier/serialNumber
+    public async Task<HttpResponseMessage> Post(string version, string deviceLibraryIdentifier, string passTypeIdentifier, string serialNumber, HttpRequestMessage message)
     {
-        //https://webServiceURL/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier/serialNumber
-        public async Task<HttpResponseMessage> Post(string version, string deviceLibraryIdentifier, string passTypeIdentifier, string serialNumber, HttpRequestMessage message)
-        {
-            string context = await message.Content.ReadAsStringAsync();
-            //object content = await message.Content.ReadAsAsync<();
-            JsonSerializer serializer = new JsonSerializer();
-            return new HttpResponseMessage(HttpStatusCode.Created);
-        }
-
-        //https://webServiceURL/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier?passesUpdatedSince=tag
-        public async Task<HttpResponseMessage> Get(string version, string deviceLibraryIdentifier, string passTypeIdentifier, HttpRequestMessage request)
-        {
-            return new HttpResponseMessage(HttpStatusCode.OK);
-        }
+      string context = await message.Content.ReadAsStringAsync();
+      //object content = await message.Content.ReadAsAsync<();
+      JsonSerializer serializer = new JsonSerializer();
+      return new HttpResponseMessage(HttpStatusCode.Created);
     }
+
+    //https://webServiceURL/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier?passesUpdatedSince=tag
+    public async Task<HttpResponseMessage> Get(string version, string deviceLibraryIdentifier, string passTypeIdentifier, HttpRequestMessage request)
+    {
+      return new HttpResponseMessage(HttpStatusCode.OK);
+    }
+
+    //https://webServiceURL/version/passes/passTypeIdentifier/serialNumber
+    public async Task<HttpResponseMessage> GetPass(string version, string passTypeIdentifier, string serialNumber)
+    {
+      return new HttpResponseMessage(HttpStatusCode.OK);
+    }
+  }
 }
