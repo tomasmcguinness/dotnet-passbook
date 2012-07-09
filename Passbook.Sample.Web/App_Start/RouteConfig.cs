c8052b8913f670c6505f720c6365efdf83026453
Tomas McGuinness <tomas@chewedpencil.net>
2012-07-09 16:55:32 +0100

    Updated the configuration to fix the issue with . characters in the routes.

--- Passbook.Sample.Web/App_Start/RouteConfig.cs
+++ Passbook.Sample.Web/App_Start/RouteConfig.cs
@@ -8,32 +8,32 @@ using System.Web.Routing;
 
 namespace Passbook.Sample.Web
 {
-    public class RouteConfig
+  public class RouteConfig
+  {
+    public static void RegisterRoutes(RouteCollection routes)
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            ////api/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier/serialNumber
-            //routes.MapHttpRoute(
-            //    name: "Registration",
-            //    routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}",
-            //    defaults: new { Controller = "Registration" });
+      //api/version/devices/deviceLibraryIdentifier/registrations/passTypeIdentifier/serialNumber
+      routes.MapHttpRoute(
+          name: "Registration",
+          routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}/{serialNumber}",
+          defaults: new { Controller = "PassRegistration", Action = "Post" });
 
-            ///api/v1/devices/d754ba022bd37181fed36ead4e7cf5d8/registrations/pass.tomasmcguinness.com
-            routes.MapHttpRoute(
-             name: "PassFetchUpdatedPasses",
-             routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}",
-             defaults: new { Controller = "PassRegistration", Action = "Get" }
-            );
+      ///api/v1/devices/d754ba022bd37181fed36ead4e7cf5d8/registrations/pass.tomasmcguinness.com
+      routes.MapHttpRoute(
+          name: "PassFetchUpdatedPasses",
+          routeTemplate: "api/{version}/devices/{deviceLibraryIdentifier}/registrations/{passTypeIdentifier}",
+          defaults: new { Controller = "PassRegistration", Action = "Get" }
+      );
 
 
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
+      routes.MapRoute(
+          name: "Default",
+          url: "{controller}/{action}/{id}",
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      );
     }
+  }
 }
