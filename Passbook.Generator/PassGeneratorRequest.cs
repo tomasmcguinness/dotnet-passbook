00567994f88288231f799b3586f0899a8ac79b79
Tomas McGuinness <tomas@chewedpencil.net>
2013-01-18 14:52:38 +0000

    Added second method for Barcode that doesn't require Alternate text

--- Passbook.Generator/PassGeneratorRequest.cs
+++ Passbook.Generator/PassGeneratorRequest.cs
@@ -198,6 +198,14 @@ namespace Passbook.Generator
             Barcode.Encoding = encoding;
             Barcode.AlternateText = altText;
         }
+        public void AddBarCode(string message, BarcodeType type, string encoding)
+        {
+          Barcode = new BarCode();
+          Barcode.Type = type;
+          Barcode.Message = message;
+          Barcode.Encoding = encoding;
+          Barcode.AlternateText = null;
+        }
         public void AddLocation(double latitude, double longitude)
         {
             AddLocation(latitude, longitude, null);
@@ -287,6 +295,7 @@ namespace Passbook.Generator
                     writer.WritePropertyName("altText");
                     writer.WriteValue(request.Barcode.AlternateText);
                 }
+
                 writer.WriteEndObject();
             }
         }
