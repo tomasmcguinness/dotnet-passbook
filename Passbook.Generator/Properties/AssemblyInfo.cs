61856ad6295de88eb8f85d9f493b34b1f6bc4bcf
Tomas McGuinness <tomas@tomasmcguinness.com>
2013-09-28 10:06:32 +0100

    Updated version and NuGet package information.

--- Passbook.Generator/Properties/AssemblyInfo.cs
+++ Passbook.Generator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Passbook.Generator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Passbook file generator for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Passbook.Generator")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("ColdBear Ltd")]
+[assembly: AssemblyProduct("Passbook Generator")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
+[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyFileVersion("0.9.0")]
