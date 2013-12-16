9b61d6411800fbd0c4936df3bb25b6ff42f74e43
Sean Ho <seanho@thoughtworks.com>
2013-12-16 17:18:40 +1000

    Sean: Fix setting DataDetectorType

---
+++ Passbook.Generator/Extensions/DataDetectorTypeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Passbook.Generator.Fields;
+
+namespace Passbook.Generator
+{
+    public static class DataDetectorTypeExtensions
+    {
+        // Dotfucated friendly enum name conversion
+        // http://stackoverflow.com/questions/483794/convert-enum-to-string
+        public static string ToSafeString(this DataDetectorType dataDetectorType)
+        {
+            switch (dataDetectorType)
+            {
+                case DataDetectorType.PKDataDetectorTypeAddress:
+                    return "PKDataDetectorTypeAddress";
+                case DataDetectorType.PKDataDetectorTypeCalendarEvent:
+                    return "PKDataDetectorTypeCalendarEvent";
+                case DataDetectorType.PKDataDetectorTypeLink:
+                    return "PKDataDetectorTypeLink";
+                case DataDetectorType.PKDataDetectorTypePhoneNumber:
+                    return "PKDataDetectorTypePhoneNumber";
+                default:
+                    return "";
+            }
+        }
+    }
+}
