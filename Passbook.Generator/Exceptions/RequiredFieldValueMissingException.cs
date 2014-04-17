c3cc8dd20509f6ae1e6b30abc6315ddc1fac1330
Tomas McGuinness <tomas@tomasmcguinness.com>
2014-04-17 19:07:48 +0100

    Added Data detector extensions

--- Passbook.Generator/Exceptions/RequiredFieldValueMissingException.cs
+++ Passbook.Generator/Exceptions/RequiredFieldValueMissingException.cs
@@ -9,16 +9,8 @@ namespace Passbook.Generator.Exceptions
     [Serializable]
     public class RequiredFieldValueMissingException : Exception
     {
-        private string key;
-
-        public RequiredFieldValueMissingException(string fieldName) : base("Missing value for field with key '" + fieldName + "'. Every field must have a value specified.") { }
-
-        public override string Message
-        {
-            get
-            {
-                return string.Format("Missing value for field [key: '{0}']. Every field must have a value specified.", key);
-            }
-        }
+        public RequiredFieldValueMissingException(string fieldName) :
+            base("Missing value for field [key: '" + fieldName + "']. Every field must have a value specified in the template or the individual pass.")
+        { }
     }
 }
