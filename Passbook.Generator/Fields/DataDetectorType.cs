9f9f7ca33b9c634b1b1e5e07374c0df82fe7b4a4
Rob van Oostenrijk <the-darkvoid@users.noreply.github.com>
2015-05-16 17:17:11 +0400

    Refactored DataDetectorType
    DataDetectorTypes is now a flags enum
    Implemented code to properly write out the requested DataDetectorTypes
    when writing a field

---
+++ Passbook.Generator/Fields/DataDetectorType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Passbook.Generator.Fields
+{
+	[Flags]
+	public enum DataDetectorTypes
+	{
+		PKDataDetectorAll = 0,
+		PKDataDetectorNone = 1,
+		PKDataDetectorTypePhoneNumber = 2,
+		PKDataDetectorTypeLink = 4,
+		PKDataDetectorTypeAddress = 8,
+		PKDataDetectorTypeCalendarEvent = 16
+	}
+}
--- Passbook.Generator/Fields/Field.cs
+++ Passbook.Generator/Fields/Field.cs
@@ -10,23 +10,21 @@ namespace Passbook.Generator.Fields
     {
         public Field()
         {
-            this.DataDetectorTypes = new List<DataDetectorType>();
+			this.DataDetectorTypes = DataDetectorTypes.PKDataDetectorAll;
         }
 
-        public Field(string key, string label)
+		public Field(string key, string label)
+			: this()
         {
             this.Key = key;
             this.Label = label;
-            this.DataDetectorTypes = new List<DataDetectorType>();
         }
 
-        public Field(string key, string label, string changeMessage, FieldTextAlignment textAligment)
+		public Field(string key, string label, string changeMessage, FieldTextAlignment textAligment)
+			: this(key, label)
         {
-            this.Key = key;
-            this.Label = label;
             this.ChangeMessage = changeMessage;
             this.TextAlignment = textAligment;
-            this.DataDetectorTypes = new List<DataDetectorType>();
         }
 
         public string Key { get; set; }
@@ -35,7 +33,7 @@ namespace Passbook.Generator.Fields
         public FieldTextAlignment TextAlignment { get; set; }
         public string AttributedValue { get; set; }
 
-        public List<DataDetectorType> DataDetectorTypes { get; private set; }
+        public DataDetectorTypes DataDetectorTypes { get; protected set; }
 
         public void Write(JsonWriter writer)
         {
@@ -82,16 +80,17 @@ namespace Passbook.Generator.Fields
 
         private void WriteDataDetectorTypes(JsonWriter writer)
         {
-            if (this.DataDetectorTypes.Count > 0)
-            {
-                StringBuilder sb = new StringBuilder();
-                sb.Append(this.DataDetectorTypes[0].ToString());
+			if (DataDetectorTypes != Fields.DataDetectorTypes.PKDataDetectorAll)
+			{
+				writer.WritePropertyName("dataDetectorTypes");
+				writer.WriteStartArray();
 
-                for (int i = 1; i < this.DataDetectorTypes.Count; i++)
-                {
-                    sb.AppendFormat(",{0}", this.DataDetectorTypes[i].ToString());
-                }
-            }
+				foreach (Enum value in Enum.GetValues(typeof(DataDetectorTypes)))
+					if (value.CompareTo(DataDetectorTypes.PKDataDetectorNone) != 0 && DataDetectorTypes.HasFlag(value))
+						writer.WriteValue(value.ToString());
+
+				writer.WriteEndArray();
+			}
         }
 
         private void Validate()
