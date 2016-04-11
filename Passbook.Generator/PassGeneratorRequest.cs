42b528fff02487e9b5eadf1394955e45f0eade36
Oliveira, Joaquim <Oliveiraj@ryanair.com>
2016-04-11 18:06:24 +0100

    Setting relevantDate and expirationDate export format with K

--- Passbook.Generator/PassGeneratorRequest.cs
+++ Passbook.Generator/PassGeneratorRequest.cs
@@ -569,7 +569,7 @@ namespace Passbook.Generator
 			if (RelevantDate.HasValue)
 			{
 				writer.WritePropertyName("relevantDate");
-				writer.WriteValue(RelevantDate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+				writer.WriteValue(RelevantDate.Value.ToString("yyyy-MM-ddTHH:mm:ssK"));
 			}
 
 			if (MaxDistance.HasValue)
@@ -727,7 +727,7 @@ namespace Passbook.Generator
 			if (ExpirationDate.HasValue)
 			{
 				writer.WritePropertyName("expirationDate");
-				writer.WriteValue(ExpirationDate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+				writer.WriteValue(ExpirationDate.Value.ToString("yyyy-MM-ddTHH:mm:ssK"));
 			}
 
 			if (Voided.HasValue)
