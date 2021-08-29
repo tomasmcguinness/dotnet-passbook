f82eb521753f99c8bdabbd4a59683ac7272aca61
Tomas McGuinness <tomas@tomasmcguinness.com>
2021-08-29 09:14:58 +0100

    Added teh event type

---
+++ Passbook.Generator/Tags/EventTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace Passbook.Generator.Tags
+{
+    public enum EventTypes
+    {
+        PKEventTypeGeneric,
+        PKEventTypeLivePerformance,
+        PKEventTypeMovie,
+        PKEventTypeSports,
+        PKEventTypeConference,
+        PKEventTypeConvention,
+        PKEventTypeWorkshop,
+        PKEventTypeSocialGathering
+    }
+}
--- Passbook.Generator/Tags/eventType.cs
+++ Passbook.Generator/Tags/eventType.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Passbook.Generator.Tags
 {
-    class eventType
+    public class EventType : SemanticTag
     {
+        private readonly EventTypes _eventType;
+
+        public EventType(EventTypes eventType) : base("eventType")
+        {
+            _eventType = eventType;
+        }
+
+        public override void WriteValue(JsonWriter writer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
