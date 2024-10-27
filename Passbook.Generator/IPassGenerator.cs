7c4cc05baf9676c797db3dd77f51146eae2bbc3a
Eimantas Vaičiūnas <eimantas@stork.software>
2024-10-27 15:21:55 +0200

    Add `IPassGenerator` interface for DI and testing
    Conform to the `IPassGenerator` interface in `PassGenerator` to allow for dependency injection and testing.
    
    Write a simple section in README to explain the purpose of the interface and how to use it.

---
+++ Passbook.Generator/IPassGenerator.cs
@@ -0,0 +1,23 @@
+public interface IPassGenerator
+{
+    /// <summary>
+    /// Creates a byte array which contains one pkpass file
+    /// </summary>
+    /// <param name="generatorRequest">
+    /// An instance of a PassGeneratorRequest</param>
+    /// <returns>
+    /// A byte array which contains a zipped pkpass file.
+    /// </returns>
+    public byte[] Generate(PassGeneratorRequest generatorRequest);
+
+    /// <summary>
+    /// Creates a byte array that can contains a .pkpasses file for bundling multiple passes together
+    /// </summary>
+    /// <param name="generatorRequests">
+    /// A list of PassGeneratorRequest objects
+    /// </param>
+    /// <returns>
+    /// A byte array which contains a zipped pkpasses file.
+    /// </returns>
+    public byte[] Generate(IReadOnlyList<PassGeneratorRequest> generatorRequests);
+}
